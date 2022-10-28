9d5a0b21f071be7027dbc16db627b69425abfa68
sahilbansal2701 <sahilbansal2701@gmail.com>
2022-10-28 10:29:38 -0400

    Intermediate

--- src/main/java/nin/Main.java
+++ src/main/java/nin/Main.java
@@ -1,4 +1,4 @@
-package wiz;
+package nin;
 
 import engine.support.FXApplication;
 import engine.support.FXFrontEnd;
@@ -6,7 +6,7 @@ import engine.support.FXFrontEnd;
 public class Main {
 
     public static void main(String[] args) {
-        FXFrontEnd app = new App("Wiz");
+        FXFrontEnd app = new App("Nin");
         FXApplication application = new FXApplication();
         application.begin(app);
     }
